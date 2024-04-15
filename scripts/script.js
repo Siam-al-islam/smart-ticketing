4cf8b82a628fa99f86493c47761f837a65a7fb1f
Siam-al-islam <siamalislam692@gmail.com>
2024-04-15 21:50:22 +0600

    seat count added

--- scripts/script.js
+++ scripts/script.js
@@ -6,14 +6,24 @@ document.body.addEventListener('click', (e) => {
 
         if (selectedSeatLength < 4 && !isSelected) {
             const selectedSeat = e.target.closest('.seat');
+            const ticketDetails = {
+                seatNo: selectedSeat.innerText,
+                class: 'Economoy',
+                seatFare: 550
+            }
+
             selectedSeat.classList.remove('bg-gray-100');
             selectedSeat.classList.add('selectedSeat');
-            substractSeat('seat-left')
+
+            substractSeat('seat-left');
+            addNewSeat('added-seat');
+            createNewElement('added-seat-detail', 'div', ticketDetails);
         } else if (isSelected) {
             const selectedSeat = e.target.closest('.seat');
             selectedSeat.classList.add('bg-gray-100');
             selectedSeat.classList.remove('selectedSeat');
-            addSeat('seat-left')
+            addSeat('seat-left');
+            removeNewSeat('added-seat');
         } else {
             console.error('You can not');
         }
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,11 +1,11 @@
 // add class 
-function addClass(elementClass, className) {
-    const element = document.getElementsByClassName(elementClass);
+function addClass(elementId, className) {
+    const element = document.getElementById(elementId);
     element.classList.add(className);
 }
 // remove class 
-function removeClass(elementClass, className) {
-    const element = document.getElementsByClassName(elementClass);
+function removeClass(elementId, className) {
+    const element = document.getElementById(elementId);
     element.classList.remove(className);
 }
 
@@ -23,4 +23,36 @@ function addSeat(elementId) {
     const seatLeft = parseInt(totalSeats.innerText);
     const currentSeats = seatLeft + 1;
     totalSeats.innerText = currentSeats;
+}
+
+// adding new seats 
+function addNewSeat(elementId) {
+    addSeat(elementId);
+}
+
+// substractSeatNewSeat 
+function removeNewSeat(elementId) {
+    substractSeat(elementId);
+}
+
+// create new element 
+function createNewElement(elementId, elementName, ticketDetails) {
+
+    const seatDetail = document.getElementById(elementId);
+    const element = document.createElement(elementName);
+    const div2 = document.createElement(elementName);
+    const div3 = document.createElement(elementName);
+    const div4 = document.createElement(elementName);
+
+    element.classList.add('ticket-detail-row', ticketDetails.seatNo)
+
+    div2.innerText = ticketDetails.seatNo;
+    div3.innerText = ticketDetails.class;
+    div4.innerText = ticketDetails.seatFare;
+
+    element.appendChild(div2);
+    element.appendChild(div3);
+    element.appendChild(div4);
+
+    seatDetail.appendChild(element);
 }
